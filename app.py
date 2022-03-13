e9d5adf733e585d8c7adb2b0b102646a22be385d
Vincent Salas Richard <contact@vincentsalas.fr>
2022-03-13 17:56:14 +0100

    removing DATABASE_URL

--- app.py
+++ app.py
@@ -15,11 +15,13 @@ app = Flask(__name__)
 
 #CONFIG_____________________________________________________________________________________
 
-# DATABASE_URL
-if os.environ.get('DATABASE_URL') is None:
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///static\\tmp\\data_val.db'
-else:
-    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
+SQLALCHEMY_DATABASE_URI = 'sqlite:///static\\tmp\\data_val.db'
+
+# # DATABASE_URL
+# if os.environ.get('DATABASE_URL') is None:
+#     SQLALCHEMY_DATABASE_URI = 'sqlite:///static\\tmp\\data_val.db'
+# else:
+#     SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
     
 # columns (from feature selection)
 columns_lst = ['SK_ID_CURR', 'PAYMENT_RATE', 'EXT_SOURCE_2', 'DAYS_BIRTH',
